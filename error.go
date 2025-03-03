6a02ed0e6f1b1efa3818dfac98a8d41ccd6fff6e
Matteo Bjornsson <matteo@blocky.rocks>
2025-03-03 14:06:00 -0700

    update fn comment

--- error.go
+++ error.go
@@ -2,6 +2,7 @@ package basm
 
 import "errors"
 
+// errWrap avoids the use of the fmt package to wrap errors.
 func errWrap(msg string, err error) error {
 	e := errors.New(msg)
 	return errors.Join(e, err)
